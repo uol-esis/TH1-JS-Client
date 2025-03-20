768bc4e04ad52f98cd51b14563ddaff67ecd0e10
Tilman Holube <tilman@holube.de>
2025-03-20 11:52:44 +0100

    Update generator to 7.12.0

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@ GID=$(id -g)
 
 docker run --rm \
   -u "${UID}":"${GID}" \
-  -v "${PWD}":/local openapitools/openapi-generator-cli:v7.11.0 generate \
+  -v "${PWD}":/local openapitools/openapi-generator-cli:v7.12.0 generate \
   -i https://raw.githubusercontent.com/uol-esis/TH1-OpenAPI/v1.0.1/openapi.yaml \
   -g javascript \
   -o /local/.
