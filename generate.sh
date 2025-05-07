6950941ece8f1b83e7d050c8bbceb8379950e0c1
Tilman Holube <tilman@holube.de>
2025-05-07 17:22:18 +0200

    add templates

--- generate.sh
+++ generate.sh
@@ -3,7 +3,8 @@ GID=$(id -g)
 
 docker run --rm \
   -u "${UID}":"${GID}" \
-  -v "${PWD}":/local openapitools/openapi-generator-cli:v7.12.0 generate \
+  -v "${PWD}":/local openapitools/openapi-generator-cli:v7.13.0 generate \
   -i https://raw.githubusercontent.com/uol-esis/TH1-OpenAPI/v1.9.0/openapi.yaml \
   -g javascript \
-  -o /local/.
+  -t /local/templates \
+  -o /local
